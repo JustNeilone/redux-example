5c5537ffce8d1f98845accaf502c915d00a8940f
Egor Kozhadey <36373985+JustNeilone@users.noreply.github.com>
2021-05-23 11:52:27 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import Posts from './components/Posts'
+import PostForm from './components/PostForm'
+import { Provider } from 'react-redux'
+import store from './store'
+
+function App() {
+  return (
+    <Provider store={store}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <PostForm />
+        <hr />
+        <Posts />
+      </div>
+    </Provider>
+  );
+}
+
+export default App;
